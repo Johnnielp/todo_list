8687ef64b45a51667329b27ab6635a5e3c28d9f5
Johnnielp <ioannislappas1992@gmail.com>
2023-09-12 16:16:22 +0300

    Ready to use

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PersonOff from "@material-ui/icons/PersonAddRounded"
+
+
+function Header() {
+  return (
+    <header>
+      <h1><PersonOff/> Jesters Bench Notepad</h1>
+    </header>
+  );
+}
+
+export default Header;
